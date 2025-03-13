0fd8a093ce97ac63fac32f98eb0b5ffc4e618a8d
umer394 <umershhzad7@gmail.com>
2025-03-13 13:22:20 +0500

    push library 0.1

--- app.py
+++ app.py
@@ -1,46 +1,66 @@
 
-print("Welcome to your Personal Library Manager!")
-print("1. Add a book")
-print("2. Remove a book  ")
-print("3. Search for a book")
-print("4. Display all books  ")
-print("5. Display statistics")
-print("6. Exit")
 
+user_data = {"name":"","age":"","phone":""}
+books = []
 start = True
 while (start):
-    input_choice = int(input("Enter your choice:"))
+    print("Welcome to your Personal Library Manager!")
+    user_name = input("Enter Your Name:")
+    user_age = input("Enter Your Age:")
+    user_phone = input("Enter Your Phone Number:")
+    if(user_name and user_phone and user_age):
+        user_data["name"] = user_name
+        user_data["age"] = user_age
+        user_data["phone"] = user_phone
+        print(user_data)
+        print("1. Add a book")
+        print("2. Remove a book  ")
+        print("3. Search for a book")
+        print("4. Display all books  ")
+        print("5. Display statistics")
+        print("6. Exit")
 
-    if(input_choice == 1):
-        addbook()
-    elif(input_choice == 2):
-        removebook()
-    elif(input_choice == 3):
-        searchbook()
-    elif(input_choice == 4):
-        displaybook()
-    elif(input_choice == 5):
-        displaystats()
-    elif(input_choice == 6):
-        quit()
-    def addbook():
-        pass
+        def addbook():
+            book_name = input("Enter the book title:")   
+            book_author = input("Enter the author:")  
+            book_model = input("Enter the publication year:")  
+            book_status = input("Have you read this book? (yes/no):") 
+            
 
-    def removebook():
-        pass
+            books.append({"Title":book_name,"Author":book_author,"Model":book_model,"Status":book_status,"User":user_name,"Age":user_age,"Phone":user_phone})
+            print("Book Added Successfully!!")
+            print(books)
 
-    def searchbook():
-        pass
+        def removebook():
+            pass
 
-    def displaybook():
-        pass
+        def searchbook():
+            pass
 
-    def displaystats():
-        pass
+        def displaybook():
+            pass
 
-    def quit():
-        start = False
-    
+        def displaystats():
+            pass
+
+
+        input_choice = int(input("Enter your choice:"))
+
+        if(input_choice == 1):
+            addbook()
+        elif(input_choice == 2):
+            removebook()
+        elif(input_choice == 3):
+            searchbook()
+        elif(input_choice == 4):
+            displaybook()
+        elif(input_choice == 5):
+            displaystats()
+        elif(input_choice == 6):
+            start = False
+    else:
+        print("Register yourself first")
+        
 
 
 
